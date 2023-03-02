a5a7e75bfc46f54b98a44d1d5ca9a203a7c8c2bb
ridmi-balasooriya <ridmi.bizy@gmail.com>
2023-03-02 12:37:18 +0400

    Change Routes of the Website - Add routes to display fetch, fetch graphQL and Render Props in different routs. - Add Style to them

--- src/App.js
+++ src/App.js
@@ -1,102 +1,167 @@
 import './App.css';
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useState, useEffect, useReducer, useRef} from 'react';
-
-
-// Render Prop Componants
-const tahoe_peaks = [
-  {name: 'Freel', elevation: 10891 },
-  {name: 'Monument', elevation: 10067},
-  {name: 'Pyramid', elevation: 9983},
-  {name: 'Tallac', elevation: 9735}
-];
-
-function List({data, renderItem, renderEmpty}) {
-  return !data.length ?
-    (renderEmpty) :
-    (
-      data.map((item) => (
-        <li key={item.name}>{renderItem(item)}</li>
-      ))
-    );
-}
-
-function ItemDetails(item) {
-  return <>{item.name} - {item.elevation}</>
-}
-
-// Fetch Componant
-function GitHubUser({name, location, avatar}) {
-  return (
-    <div>
-      <h3>{name}</h3>
-      <p>{location}</p>
-      <img src={avatar} height='100' alt={name} />
-    </div>
-  );
-}
-
-// Fetch GraphQL Componant
-const query = `
-  query{
-    allLifts{
-      name
-      elevationGain
-      status
-    }
-  }
-`;
-
-const opts = {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json'},
-  body: JSON.stringify({ query })
-}
-
-function GraphQLUser ({name, elevationGain, status}) {
-  return(
-    <div>
-      <h5>{name}</h5>
-      <p>{elevationGain} : {status}</p>
-    </div>
-  );  
-}
 
 export function Home() {
   return (
     <div>
       <nav>
         <Link to='/'>Home</Link>
-        <Link to='/about'>About Us</Link>
-        <Link to='/contact'>Contact Us</Link>
+        <Link to='/fetch-data'>Fetch Data</Link>
+        <Link to='/fetch-graphql'>Fetch GraphQL</Link>
+        <Link to='/render-prop'>Render Prop</Link>
       </nav>
     </div> 
   )
 }
 
-export function History() {
+export function RenderProp() {
+  // Render Prop Componants
+  const tahoe_peaks = [
+    {name: 'Freel', elevation: 10891 },
+    {name: 'Monument', elevation: 10067},
+    {name: 'Pyramid', elevation: 9983},
+    {name: 'Tallac', elevation: 9735}
+  ];
+
+  function List({data, renderItem, renderEmpty}) {
+    return !data.length ?
+      (renderEmpty) :
+      (
+        data.map((item) => (
+          <li key={item.name}>{renderItem(item)}</li>
+        ))
+      );
+  }
+
+  function ItemDetails(item) {
+    return <>{item.name} - {item.elevation}</>
+  }
+  
   return(
-    <div>
-      <h1>History</h1>
+    <div className="App">      
+      <Home /> 
+      <div className='peak_div'>
+          <h2>Render Props - Tahoe Peaks</h2>
+        <ul>
+          <List 
+            data={tahoe_peaks} 
+            renderEmpty='<h3>The List is Empty</h3>'
+            renderItem = {ItemDetails}
+          />
+        </ul>
+      </div>
     </div>
   )
 }
 
-export function About() {
+export function FetchData() {
+  // Fetch Componant
+  function GitHubUser({name, location, avatar}) {
+    return (
+      <div>
+        <h3>{name}</h3>
+        <p>{location}</p>
+        <img src={avatar} height='100' alt={name} />
+      </div>
+    );
+  }
+
+  // Fetch Data
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(`https://api.github.com/users/moonhighway`)
+    .then((response) => response.json())
+    .then((data) => setData(data))
+    .then(() => setLoading(false))
+    .catch((e) => setError(e));
+  }, []);
+
+  if(loading) return <div className="App"><h1>Loading...</h1></div>;
+  if(error) return <div className="App"><pre>{JSON.stringify(error)}</pre></div>;
+  if(!data) return null;
+
   return (
     <div className="App">
+      
       <Home />
-      <h1>About Us</h1>
-      <Outlet />
+      <div>
+        <h2>Fetch Data</h2>   
+        <GitHubUser 
+          name={data.name} 
+          location={data.location} 
+          avatar={data.avatar_url} 
+        />
+      </div>
     </div>
   )
 }
 
-export function Contact() {
+export function FetchGraphQL() {
+  // Fetch GraphQL Componant
+  const query = `
+    query{
+      allLifts{
+        name
+        elevationGain
+        status
+      }
+    }
+  `;
+
+  const opts = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({ query })
+  }
+
+  function GraphQLUser ({name, elevationGain, status}) {
+    return(
+      <div>
+        <h5>{name}</h5>
+        <p>{elevationGain} : {status}</p>
+      </div>
+    );  
+  }
+
+  // GraphQL Data
+  const [dataGql, setDataGql] = useState(null);
+  const [errorGql, setErrorGql] = useState(null);
+  const [loadingGql, setLoadingGql] = useState(false);
+
+  useEffect(() => {
+    setLoadingGql(true);
+
+    fetch('https://snowtooth.moonhighway.com/', opts)
+    .then((response) => response.json())
+    .then((data) => setDataGql(data))
+    .then(() => setLoadingGql(false))
+    .catch((e) => setErrorGql(e));
+  }, []);  
+
+  if(loadingGql) return <div className="App"><h1>Loading...</h1></div>;
+  if(errorGql) return <div className="App"><pre>{JSON.stringify(errorGql)}</pre></div>;
+  if(!dataGql) return null;
+  
   return(
     <div className="App">
       <Home />
-      <h1>Contact Us</h1>
+      <div>
+        <h2>GraphQL Data</h2>   
+        <div className='gql_div'>
+          {
+            dataGql.data.allLifts.map((lift) => (
+              <GraphQLUser name={lift.name} elevationGain={lift.elevationGain} status={lift.status} />
+            ))
+          }
+        </div>
+        <hr />
+      </div>
     </div>
   )
 }
@@ -166,134 +231,61 @@ export function App() {
     hexColor.current.value = '';
   }
   
-  // Fetch Data
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-
-    fetch(`https://api.github.com/users/moonhighway`)
-    .then((response) => response.json())
-    .then((data) => setData(data))
-    .then(() => setLoading(false))
-    .catch((e) => setError(e));
-  }, []);
-
-  // GraphQL Data
-  const [dataGql, setDataGql] = useState(null);
-  const [errorGql, setErrorGql] = useState(null);
-  const [loadingGql, setLoadingGql] = useState(false);
-
-  useEffect(() => {
-    setLoadingGql(true);
-
-    fetch('https://snowtooth.moonhighway.com/', opts)
-    .then((response) => response.json())
-    .then((data) => setDataGql(data))
-    .then(() => setLoadingGql(false))
-    .catch((e) => setErrorGql(e));
-  }, []);
-
-  if(loading) return <h1>Loading...</h1>;
-  if(error) return <pre>{JSON.stringify(error)}</pre>;
-  if(!data) return null;
-
-  if(loadingGql) return <h1>Loading...</h1>;
-  if(errorGql) return <pre>{JSON.stringify(errorGql)}</pre>;
-  if(!dataGql) return null;
-
-
-
   return (   
     
-    <div className="App">
-      
+    <div className="App">      
       <Home />
+      <h2>React Hooks - UseState | UseEffect | UseReducer | UseRef</h2>
+      <div className='app-div'>       
 
-      <div className='peak_div'>
-        <h2>Render Props - Tahoe Peaks</h2>
-        <ul>
-          <List 
-            data={tahoe_peaks} 
-            renderEmpty='<h3>The List is Empty</h3>'
-            renderItem = {ItemDetails}
+        <h3>Custom Hook</h3>
+        <form onSubmit={submitCustom}>
+          <input type='text' placeholder='Color title' {...titleProps} />
+          <input type='color' {...colorProps} />
+          <button>Add</button>
+        </form>
+      </div>
+
+      <div className='app-div'>
+        <h3>UseState</h3>
+        <form onSubmit={submitControll}>
+          <input type='text' placeholder='Color title' 
+            value={title} 
+            onChange={(event) => setTitle(event.target.value)} 
           />
-        </ul>
+          <input type='color' 
+            value={color} 
+            onChange={(event) => setColor(event.target.value)} 
+          />
+          <button>Add</button>
+        </form>
       </div>
 
-      <hr/>
-
-      <div>
-        <h2>GraphQL Data</h2>   
-        <div className='gql_div'>
-          {
-            dataGql.data.allLifts.map((lift) => (
-              <GraphQLUser name={lift.name} elevationGain={lift.elevationGain} status={lift.status} />
-            ))
-          }
-        </div>
-        <hr />
+      <div className='app-div'>
+        <h3>UseRef</h3>
+        <form onSubmit={submit}>
+          <input type='text' placeholder='Color title...' ref={textTitle} />
+          <input type='color' ref={hexColor} />
+          <button>Add</button>
+        </form>
       </div>
 
-      <div>
-        <h2>Fetch Data</h2>   
-        <GitHubUser 
-          name={data.name} 
-          location={data.location} 
-          avatar={data.avatar_url} 
-        />
-        <hr />
+      <div className='app-div'>
+        <h3>UseReducer</h3>
+        <input type='checkbox' onChange={setChecked} value={checked} />
+        <label>{checked ? 'Checked' : 'Not Check'}</label>
       </div>
 
+      <div className='app-div'>
+        <h3>UseState & UseEffect</h3>
+        <h1>Current emtion is {emtion}</h1>
+        <button onClick={ () => setEmotion('Sad')}>Sad</button>
+        <button onClick={ () => setEmotion('Happy')}>Happy</button>
+        <button onClick={ () => setEmotion('Angry')}>Angry</button>
 
-      <form onSubmit={submitCustom}>
-        <input type='text' placeholder='Color title' {...titleProps} />
-        <input type='color' {...colorProps} />
-        <button>Add</button>
-      </form>
-
-      <hr />
-
-      <form onSubmit={submitControll}>
-        <input type='text' placeholder='Color title' 
-          value={title} 
-          onChange={(event) => setTitle(event.target.value)} 
-        />
-        <input type='color' 
-          value={color} 
-          onChange={(event) => setColor(event.target.value)} 
-        />
-        <button>Add</button>
-      </form>
-
-      <hr />
-
-      <form onSubmit={submit}>
-        <input type='text' placeholder='Color title...' ref={textTitle} />
-        <input type='color' ref={hexColor} />
-        <button>Add</button>
-      </form>
-
-      <hr />
-
-      <input type='checkbox' onChange={setChecked} value={checked} />
-      <label>{checked ? 'Checked' : 'Not Check'}</label>
-
-      <hr />
-
-      <h1>Current emtion is {emtion}</h1>
-      <button onClick={ () => setEmotion('Sad')}>Sad</button>
-      <button onClick={ () => setEmotion('Happy')}>Happy</button>
-      <button onClick={ () => setEmotion('Angry')}>Angry</button>
-
-      <hr />
-
-      <h2>Current Secondary emotion is {secondary}</h2>
-      <button onClick={() => setSecondary('Grateful')}>Grateful</button>
-
-      <hr />
+        <h2>Current Secondary emotion is {secondary}</h2>
+        <button onClick={() => setSecondary('Grateful')}>Grateful</button>
+      </div>
 
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { App, About, Contact, History } from './App';
+import { App, FetchData, FetchGraphQL, RenderProp } from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,10 +9,9 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />} />
-      <Route path='/about' element={<About />}>
-        <Route path='history' element={<History />} />
-      </Route>
-      <Route path='/contact' element={<Contact />} />
+      <Route path='/fetch-data' element={<FetchData />} />      
+      <Route path='/fetch-graphql' element={<FetchGraphQL />} />
+      <Route path='/render-prop' element={<RenderProp />} />
     </Routes>
 
   </BrowserRouter>
